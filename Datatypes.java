e142cb4810cc17fd974fda1e5f07d7a5085075d5
kalaivani20122002 <145123214+kalaivani20122002@users.noreply.github.com>
2024-12-27 16:03:25 +0530

    Datatypes - Task2

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+package Day5;
+
+public class Datatypes {
+	public static void main(String[]args) {
+		String name1="Liam";
+		String name2="Maya";
+		String adjective1 ="cheerful";
+		String adjective2 ="productive";
+		String noun1="kittens";
+		String noun2="pancakes";
+		String verb1="sing";
+		String noun3="bongos";
+		String noun4="trees";
+		String adjective3 ="shocked";
+		String place1="the beach";
+		String noun5="yogurt";
+		String number="2077";
+		String noun6="unicorns";
+		
+		String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+		System.out.println(story);
+	}
+
+}
